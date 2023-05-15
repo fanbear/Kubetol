ed0e50a8abb8b92c54ff2c10b043c479ce55bcb5
Yura <fanbearytb@gmail.com>
2023-05-15 16:45:03 +0300

    main page mob adaptive

--- .history/js/main_20230404155243.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Language list open
-    const langList = document.querySelector('.language-list');
-    const langBtn = document.querySelector('.header-list__open');
-
-    langBtn.addEventListener('click', (event) => {
-        event.style.transform = 'rotate(90deg)';
-    })
-})
--- .history/js/main_20230511171942.js
+++ .history/js/main_20230511171942.js
@@ -232,14 +232,7 @@ $(document).ready(function () {
         dots: true,
         autoplay: false,
         autoplayTimeout: 5000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-                autoplay: false,
-            }
-        }
+        autoplayHoverPause: true
     });
 
     //Kubetelo shipped sldier
